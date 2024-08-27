011f09eaf15b34d240a4c4e11c2fb8b99c7c1d79
Babatunde Valentine Onabajo <36359216+BabatundeOnabajo@users.noreply.github.com>
2024-08-27 13:52:06 +0100

    Create dataframeforeachcountryinthe2024olympics.r
    Added this file, the first part of the data frame as well as a comment relating to the fact that the names of each country are taken from the official Olympics website, and do not imply any political view on the part of the author.

---
+++ dataframeforeachcountryinthe2024olympics.r
@@ -0,0 +1,2 @@
+# The names of each country is taken from the official Olympics website. It does not imply any political view of the author of this code.
+olympicMedalStatisticsByCountry <- data.frame(COUNTRY = c("", "", "", ""), MEDALCOUNT = c(), POPULATION = c(), GDP = c()) # We create a data frame to set this up for our Poisson regression modelling later.
